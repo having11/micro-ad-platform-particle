df25626f75e3056ff47023b9332fb28b23a60941
having11 <gatoninja236@gmail.com>
2023-09-24 10:48:22 -0500

    Initial files

---
+++ micro-ad-display/src/micro-ad-display.ino
@@ -0,0 +1,14 @@
+/*
+ * Project micro-ad-display
+ * Description: Display GIF-based ads that can be updated OTA via Asset OTA on a Particle Photon 2 with metrics
+ * Author: Evan Rust
+ * Date: 24-09-2023
+ */
+
+void setup() {
+    
+}
+
+void loop() {
+
+}
